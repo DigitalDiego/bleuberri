4a274e54e6ddb39f6bb6ce1accc5a8b2fb58e324
Diego Hernandez <diegohxxiv@gmail.com>
2022-12-26 14:10:58 -0800

    Mobile hero section fixed

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function Hero() {
   return (
     <div className='relative w-full h-[85vh] 2xl:h-[90vh]'>
-      <video className='w-full h-full object-cover' src="/videos/hero.mp4" loop autoPlay muted></video>
+      <video className='w-full h-full object-cover hidden lg:inline' src="/videos/hero.mp4" loop autoPlay muted/>
+      <Image
+        className='w-full h-full object-cover lg:hidden'
+        src='/images/hero.jpg'
+        alt='living room'
+        width={300}
+        height={300}
+      />
       <div className='absolute top-0 right-0 z-[1000] w-full h-full bg-gray-800/50 grid place-items-center'>
         <p data-aos='fade-up' className='text-5xl font-lobster text-[linen]/50'>BleuBerri</p>
       </div>
